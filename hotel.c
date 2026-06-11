065f7d4f4d03a155cced83d90e2da932aaa391bb
Rodrigo Carvalho <rodrigoalvesdecarvalho21@gmail.com>
2026-06-11 11:12:26 -0300

    Update hotel.c

--- hotel.c
+++ hotel.c
@@ -6,14 +6,14 @@
 #define MAX_QUARTOS 100
 #define MAX_RESERVAS 100
 
-// Estrutura do Hóspede
+///// Estrutura do Hóspede /////
 struct Hospede {
     int id;
     char nome[100];
     char telefone[20];
 };
 
-// Estrutura do Quarto
+///// Estrutura do Quarto /////
 struct Quarto {
     int numero;
     char tipo[30];
@@ -21,7 +21,7 @@ struct Quarto {
     int ocupado; // 0 = Livre, 1 = Ocupado
 };
 
-// Estrutura de Reserva
+///// Estrutura de Reserva /////
 struct Reserva {
    int codigoReserva;
    int codigoHospede;
@@ -31,14 +31,44 @@ struct Reserva {
    int ativa;
 };
 
-/////////////////////
+///// Vetores /////
 
-////////////////////
-
-Hospede[MAX_HOSPEDES];
-Quarto[MAX_QUARTOS];
+Hospedes[MAX_HOSPEDES];
+Quartos[MAX_QUARTOS];
 Reservas[MAX_RESERVAS];
 
-int totaldehospedes = 0;
-int totaldequartos = 0;
-int totaldereservas = 0;
+int totalHospedes = 0;
+int totalQuartos = 0;
+int totalReservas = 0;
+
+///// Módulo de Hóspedes /////
+
+void cadastrashospede() {
+    if(totalHospedes >= MAX_HOSPEDES){
+        printf("Limite atingido!\n");
+        return;
+    }
+
+    printf("\nCodigo: ");
+    scanf("%d", &hospedes[totalHospedes].codigo);
+
+    getchar();
+
+    printf("Nome: ");
+    fgets(hospedes[totalHospedes].nome,100,stdin);
+
+    printf("CPF: ");
+    fgets(hospedes[totalHospedes].cpf,20,stdin);
+
+    hospedes[totalHospedes].ativo = 1;
+
+    totalHospedes++;
+
+    printf("Hospede Cadastrado!\n");
+
+}
+
+void listarHospedes() {
+    int i;
+
+    printf("\n ===== HOSPEDES =====\m");
